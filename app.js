b054551026e8ea2ae0231f19d3aa17600f82cba6
Jai Parmar <jaiparmar207@gmail.com>
2020-05-16 19:09:18 +0530

    ejs view template implemented

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
 const path = require('path');
 
-const rootDir = require('./util/path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const routes = require('./routes')
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
 //create application
 const app = express();
 
+//global configiration value.
+//app.set('view engine', 'pug'); // also have to tell where to find it.
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+//bodyparser for request body... data...
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminRoutes);
+//routes..
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, 'views','404.html'));
+
+//unknown routes..
+app.use((req, res, next ) => {
+    res.status(404).render('404', {pageTitle: 'Page Not Found!'});
 });
+
+//active the server...
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -3,12 +3,15 @@ const path = require('path');
 const express = require('express');
 
 const router = express.Router();
+const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 router.get("/", (req, res, next) => {
 //   console.log("in the home");
 //   res.send("<h1>Hello form Express!</h1>");
-    res.sendFile(path.join(__dirname,'../','views','shop.html'));
-});
+    const products = adminData.products;
 
+    res.render('shop', {prods: products, docTitle:'Shop'});
+});
 
 module.exports = router;
